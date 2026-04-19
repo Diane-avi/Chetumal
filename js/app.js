81a942d587f96a78d288c96133d55ce264956279
Diane-avi <dianelinoavila@gmail.com>
2026-04-19 10:17:55 -0600

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Gracias por tu mensaje. Pronto nos pondremos en contacto.");
-        form.reset();
-    });
-});
